851946fdf9557924c1b2549b5e954a1b5444638f
acbernardez17 <acbernardez17@gmail.com>
2020-11-27 19:10:02 +0100

    Statistical Grid Structure done!

--- README.md
+++ README.md
@@ -7,3 +7,15 @@
         Ejemplo: 
             El de las estadísticas: statistical_layot.xml
             
+##    Modificaciones hechas por Antonio
+
+        Creado: 
+            +Menu folder
+            +options_menu.xml
+            +statistical_layout.xml
+           + Métodos para añadir el menú de opciones en el main_activity
+           
+       He añadido un date picker en mi layout, no sé como interactuará con vuestros layouts pero no debería haber problema.
+           he seeguido todos los pasos de aquí: https://github.com/jhonnyx2012/HorizontalPicker
+           y he modificado los colores.
+           
--- app/src/main/java/com/aad/esei/uvigo/MainActivity.java
+++ app/src/main/java/com/aad/esei/uvigo/MainActivity.java
@@ -1,19 +1,56 @@
 package com.aad.esei.uvigo;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.view.WindowInsetsController;
 
-public class MainActivity extends AppCompatActivity {
+import com.github.jhonnyx2012.horizontalpicker.DatePickerListener;
+import com.github.jhonnyx2012.horizontalpicker.HorizontalPicker;
+
+import org.joda.time.DateTime;
+
+public class MainActivity extends AppCompatActivity implements DatePickerListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.statistical_layout);
+
+        // Date picker
+        HorizontalPicker picker = (HorizontalPicker) findViewById(R.id.datePicker);
+        picker
+                .setDayOfWeekTextColor(Color.CYAN)
+                .setTodayButtonTextColor(Color.rgb(255, 143, 0))
+                .setTodayDateTextColor(Color.BLACK)
+                .setUnselectedDayTextColor(Color.YELLOW)
+                .setMonthAndYearTextColor(Color.RED)
+                .setDateSelectedTextColor(Color.BLACK)
+                .setDateSelectedColor(Color.rgb(255, 143, 0))
+                .setListener(this)
+                .showTodayButton(true)
+                .setBackgroundColor(Color.rgb(42, 42, 42));
+        picker.init();
+        picker.setDate(new DateTime());
+
     }
 
+    //Datepicker Menu
+    @Override
+    public void onDateSelected(@NonNull final DateTime dateSelected) {
+        // log it for demo
+        Log.i("HorizontalPicker", "Selected date is " + dateSelected.toString());
+    }
+
+    // Options menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
