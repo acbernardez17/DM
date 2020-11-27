8144d08c81220351b95a0712dce328aef5f4deb5
acbernardez17 <acbernardez17@gmail.com>
2020-11-27 17:04:42 +0100

    created: options_menu.xml statistical_layout.xml menuFolder

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-## Car Manager
+# Car Manager
+
+
+##   Nombre de los layouts
+        NombreEspecífico_layout.xml
+        
+        Ejemplo: 
+            El de las estadísticas: statistical_layot.xml
+            
--- app/src/main/java/com/aad/esei/uvigo/MainActivity.java
+++ app/src/main/java/com/aad/esei/uvigo/MainActivity.java
@@ -3,12 +3,21 @@ package com.aad.esei.uvigo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.statistical_layout);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return true;
     }
 }
