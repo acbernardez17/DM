bcc64fb9a6f42bd45f009ee423a380951a7e5fbb
Diego <dmfernandez17@esei.uvigo.es>
2020-11-28 20:44:11 +0100

    Creado: 	+Clase CustomList para crear la lista de gastos 	+Iconos de la lista y botones (<nombre>_svg: inicio, fin, gasol, taller, home_icon, profile_icon, stats_icon) 	+Border_spinner.xml para dar formato al Spinner(/drawable) 	+listview_elem.xml (layout de cada elemento de la lista) 10 en total
    Modificado:
    	+MainActivity: codigo para rellenar la lista con los gastos
    	+main_layout: layout de la vista del main
    	+colors.xml: añadido color azul de los iconos (@color/blue)
    	+build.gradle(.app): añadido a dependencies el import para usar el botón flotante (implementation 'androidx.appcompat:appcompat:1.2.0')
    4 en total
    
    La lista creada es estática por el momento. Más adelante se puede usar un método propio para actualizarlo y cambiar los arrays por otro tipo de listas.

---
+++ app/src/main/java/com/aad/esei/uvigo/CustomList.java
@@ -0,0 +1,62 @@
+package com.aad.esei.uvigo;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class CustomList extends ArrayAdapter{
+    private String[] tipoGastos;
+    private String[] FechaGastos;
+    private String[] KmGastos;
+    private String[] CantidadGastos;
+    private Integer[] idIcono;
+    public CustomList(Activity context, String[] tipoGastos, String[] FechaGastos, String[] KmGastos, String[] CantidadGastos, Integer[] idIcono) {
+        super(context, R.layout.listview_elem, tipoGastos);
+        this.tipoGastos = tipoGastos;
+        this.FechaGastos = FechaGastos;
+        this.KmGastos = KmGastos;
+        this.CantidadGastos = CantidadGastos;
+        this.idIcono = idIcono;
+
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View elemento=convertView;
+        final Activity ACTIVITY = (Activity) this.getContext();
+        LayoutInflater inflater = ACTIVITY.getLayoutInflater();
+        if(convertView==null)
+            elemento = inflater.inflate(R.layout.listview_elem, null, true);
+        final TextView LBL_TIPO_GASTO = (TextView) elemento.findViewById(R.id.lbl_tipo_gasto);
+        final TextView LBL_FECHA_GASTO = (TextView) elemento.findViewById(R.id.lbl_fecha_gasto);
+        final TextView LBL_KM_GASTO = (TextView) elemento.findViewById(R.id.lbl_km_gasto);
+        final TextView LBL_CANT_GASTO = (TextView) elemento.findViewById(R.id.lbl_cantidad_gasto);
+        final ImageView IMG_GASTO = (ImageView) elemento.findViewById(R.id.img_icono_gasto);
+        if (position==0) {
+            LBL_TIPO_GASTO.setText("");
+            LBL_FECHA_GASTO.setText("");
+            LBL_KM_GASTO.setText("");
+            LBL_CANT_GASTO.setText("");
+            IMG_GASTO.setImageResource(R.drawable.inicio_svg);
+            return elemento;
+        }else if(position==(tipoGastos.length-1)){
+            LBL_TIPO_GASTO.setText("");
+            LBL_FECHA_GASTO.setText("");
+            LBL_KM_GASTO.setText("");
+            LBL_CANT_GASTO.setText("");
+            IMG_GASTO.setImageResource(R.drawable.fin_svg);
+            return elemento;
+        }else {
+            LBL_TIPO_GASTO.setText(tipoGastos[position]);
+            LBL_FECHA_GASTO.setText(FechaGastos[position]);
+            LBL_KM_GASTO.setText(KmGastos[position]);
+            LBL_CANT_GASTO.setText(CantidadGastos[position]);
+            IMG_GASTO.setImageResource(idIcono[position]);
+            return elemento;
+        }
+    }
+}
--- app/src/main/java/com/aad/esei/uvigo/MainActivity.java
+++ app/src/main/java/com/aad/esei/uvigo/MainActivity.java
@@ -8,13 +8,78 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView listView;
 
+    private String[] tipoGastos= {
+            "",
+            "Reabastecimiento",
+            "Taller",
+            "Taller",
+            "Reabastecimiento",
+            "Reabastecimiento",
+            ""
+    };
+    private String[] FechaGastos= {
+            "",
+            "10-11-2020",
+            "15-11-2020",
+            "20-11-2020",
+            "20-11-2020",
+            "28-11-2020",
+            ""
+    };
+    private String[] KmGastos= {
+            "",
+            "200.000 km",
+            "200.100 km",
+            "200.150 km",
+            "200.200 km",
+            "200.250 km",
+            ""
+    };
+    private String[] CantidadGastos= {
+            "",
+            "50,00€",
+            "100,00€",
+            "80,00€",
+            "45,00€",
+            "60,00€",
+            ""
+    };
+    private Integer[] idIcono = {
+            0,
+            R.drawable.gasol_svg,
+            R.drawable.taller_svg,
+            R.drawable.taller_svg,
+            R.drawable.gasol_svg,
+            R.drawable.gasol_svg,
+            0
+
+    };
+
+    private String[] perfiles ={
+            "Perfil1",
+            "Perfil2",
+            "Perfil3"
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+
+
+        ListView listView = (ListView)findViewById(R.id.list_icons);
+        CustomList list =  new CustomList(this,tipoGastos, FechaGastos, KmGastos, CantidadGastos, idIcono);
+        listView.setAdapter(list);
+
+        Spinner SpPerfil = (Spinner) findViewById(R.id.sp_perfil);
+        SpPerfil.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item,perfiles));
+
     }
 
     // Options menu
